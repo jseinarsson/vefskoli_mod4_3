571b62a0eff7e344865d8b7c9fce4d578cece0a8
Jakob Sturla <jseinarsson@gmail.com>
2020-11-01 22:14:01 +0000

    final commit

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 let countries = [];
 
+// Load API with information on world countries
 async function loadCountries() {
     const res = await fetch('https://restcountries.eu/rest/v2/all');
 
+    //Throws error is result is not OK (results are not OK if they don't return a code between 200 and 299), else it returns the data as JSON
     if (!res.ok) {
         throw new Error('Network response error');
     } else {
-        return countryObj = await res.json();
+        return await res.json();
     }
 }
 
+// On dom load, the loadcountries function is run and tries to assign the JSON string to the countries object, else it console logs the error...
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         countries = await loadCountries();
@@ -27,11 +30,17 @@ const result = document.getElementById('result');
 const checkBoxes = document.querySelectorAll('input');
 
 dart.addEventListener('click', e => {
-    do {
-        let countryObj = randomCountry();
-        let countrySelection = checkSelection(countryObj.region);
-    } while (!countrySelection);
+    // Gets a random country and checks whether the selection is valid
+    let countryObj = randomCountry();
+    let countryValid = checkSelection(countryObj.region);
     
+    // If countryValid is false, it will continue to run until it gets a valid region
+    while (!countryValid) {
+        countryObj = randomCountry();
+        countryValid = checkSelection(countryObj.region);
+    }
+    
+    // Adds/resets the inner HTML of result, adding the country name and button to view more.
     result.innerHTML = (`<h3>You should visit ${countryObj.name}</h3><button id="readmore">Find out more</button>`);
     readmore = document.getElementById('readmore');
 
@@ -40,6 +49,7 @@ dart.addEventListener('click', e => {
     });
 });
 
+// Checks the region of the randomly generated country against the checkboxes and return true if the region is checked or false if not checked
 function checkSelection(randomRegion) {
     let placesToGo = [];
 
@@ -56,10 +66,12 @@ function checkSelection(randomRegion) {
     }
 }
 
+// Function that returns a random country from the countries json list
 function randomCountry() {
     return (countries[Math.floor(Math.random() * countries.length)]);
 }
 
+// Builder when pressing "find out more"
 function countryMoreInfo(country) {
     result.innerHTML += `<ul>
     <li><img src="${country.flag}" alt="The flag of ${country.name}"></li>
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Module 4: Javascript - API Consumption
+
+I used the [Rest Countries API](https://restcountries.eu) to create a "Where to visit" randomizer, so that people can dream of foreign soil during this pandemic.
+
+The information is loaded with async/await and the user can get a randomized country from the list. Once a country has been loaded, the user can choose to view more information. The user can also filter out world regions they do not want to visit.
+
+I only did super minimal styling on this... so it's a little bit ugly.
+
+I definitely have ideas of how to take this further, by allowing users to add countries to a wishlist, maybe connecting to the Google Maps API and showing the location on a map... and yeah, making it prettier overall. But that has to wait for a better moment in time.
